3c2e493ef39287a24a0f68bb6a7c99d2498680c9
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-08-11 11:03:17 -0600

    Add project files.

---
+++ ProsperDaily/MauiProgram.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace ProsperDaily
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Roboto-Black.ttf", "Strong");
+                    fonts.AddFont("LibreFranklin-Regular.ttf", "Regular");
+                });
+
+
+            return builder.Build();
+        }
+    }
+}
