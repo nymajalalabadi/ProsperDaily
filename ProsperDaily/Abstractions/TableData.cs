3692520148dacb959f07ecac3693f14546771882
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-08-13 09:10:56 -0600

    Added Base Repository

---
+++ ProsperDaily/Abstractions/TableData.cs
@@ -0,0 +1,11 @@
+﻿using SQLite;
+
+namespace ProsperDaily.Abstractions
+{
+    public class TableData
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+    }
+
+}
---
+++ ProsperDaily/Repositories/BaseRepository.cs
@@ -0,0 +1,132 @@
+﻿using ProsperDaily.Abstractions;
+using SQLite;
+using SQLiteNetExtensions.Extensions;
+using System.Linq.Expressions;
+
+namespace ProsperDaily.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : TableData, new()
+    {
+        SQLiteConnection _connection;
+        public string StatusMessage { get; set; }
+
+        public BaseRepository()
+        {
+            _connection = new SQLiteConnection(Constants.DatabasePath, Constants.Flags);
+            _connection.CreateTable<T>();
+        }
+
+        public void DeleteItem(T item)
+        {
+            try
+            {
+                //_connection.Delete(item);
+                _connection.Delete(item, true);
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = $"Error: {ex.Message}";
+            }
+        }
+
+        public void Dispose()
+        {
+            _connection.Close();
+        }
+
+        public T GetItem(int id)
+        {
+            try
+            {
+                return _connection.Table<T>().FirstOrDefault(x => x.Id == id);
+            }
+            catch (Exception ex)
+            {
+                StatusMessage =
+                     $"Error: {ex.Message}";
+            }
+            return null;
+        }
+
+        public T GetItem(Expression<Func<T, bool>> predicate)
+        {
+            try
+            {
+                return _connection.Table<T>().Where(predicate).FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = $"Error: {ex.Message}";
+            }
+            return null;
+        }
+
+        public List<T> GetItems()
+        {
+            try
+            {
+                return _connection.Table<T>().ToList();
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = $"Error: {ex.Message}";
+            }
+            return null;
+        }
+
+        public List<T> GetItems(Expression<Func<T, bool>> predicate)
+        {
+            try
+            {
+                return _connection.Table<T>().Where(predicate).ToList();
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = $"Error: {ex.Message}";
+            }
+            return null;
+        }
+
+        public void SaveItem(T item)
+        {
+            int result = 0;
+            try
+            {
+                if (item.Id != 0)
+                {
+                    result = _connection.Update(item);
+                    StatusMessage = $"{result} row(s) updated";
+                }
+                else
+                {
+                    result = _connection.Insert(item);
+                    StatusMessage = $"{result} row(s) added";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = $"Error: {ex.Message}";
+            }
+        }
+
+        public void SaveItemWithChildren(T item, bool recursive = false)
+        {
+            _connection.InsertWithChildren(item, recursive);
+        }
+
+        public List<T> GetItemsWithChildren()
+        {
+            try
+            {
+                return _connection.GetAllWithChildren<T>().ToList();
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = $"Error: {ex.Message}";
+            }
+            return null;
+        }
+    }
+
+}
